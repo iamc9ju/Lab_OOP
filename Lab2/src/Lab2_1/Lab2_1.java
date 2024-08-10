4cb080c71c7fb9d8c7f49cca6d0e8e4195d1a713
CHAO <149712568+iamc9ju@users.noreply.github.com>
2024-08-10 13:40:11 +0700

    Update Lab2_1.java

--- Lab2/src/Lab2_1/Lab2_1.java
+++ Lab2/src/Lab2_1/Lab2_1.java
@@ -1,34 +1,23 @@
 package Lab2_1;
 import java.util.Scanner;
 public class Lab2_1 {
-
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Please enter your number : ");
-		String inputString = scanner.nextLine();
-		int inputInt = Integer.parseInt(inputString);
-		displayOutput(inputString,summation(inputInt));
-		
-
-	}
-	static int summation(int inputInt) {
-		int sum = 0;
-		while(inputInt > 0) {
-			sum += inputInt%10;
-			inputInt /= 10;
-		}
-		return sum;
-	}
-	static void displayOutput(String inputString,int sum) {
-		System.out.print(inputString + "=");
-		for(int i = 0; i<inputString.length();i++) {
-			System.out.print(inputString.charAt(i));
-			if(i<inputString.length()-1) {
-				System.out.print("+");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please input the number : ");
+		String inputString = sc.nextLine();
+		System.out.print(inputString + " = ");
+		int result = 0  ;
+		for(int i =0 ; i<inputString.length();i++) {
+			result += Character.getNumericValue(inputString.charAt(i));
+			System.out.print(inputString.charAt(i)); 
+			if(i != (inputString.length() -1)) {
+				System.out.print(" + ");
 			}else {
-				System.out.print("=");
+				System.out.print(" = ");
 			}
 		}
-		System.out.print(sum);
+		System.out.println(result);
+		
 	}
+}
 }
