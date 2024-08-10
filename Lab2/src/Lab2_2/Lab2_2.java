cf583244da4dff7b2be3a859409ec59491b6ed88
CHAO <149712568+iamc9ju@users.noreply.github.com>
2024-08-10 14:39:51 +0700

    Update Lab2_2.java

--- Lab2/src/Lab2_2/Lab2_2.java
+++ Lab2/src/Lab2_2/Lab2_2.java
@@ -1,44 +1,43 @@
-package lab2_2;
-
+package Lab2_2;
 
 public class Lab2_2 {
-
 	public static void main(String[] args) {
+		int i,j;
 		final int SIZE = 5;
-		
-		int[][] grid = new int[SIZE][SIZE];
-		for(int i = 0; i < SIZE ; i++) {
-			for(int j = 0; j< SIZE ; j++) {
+		int [][]grid = new int[SIZE][SIZE];
+		for(i = 0;i < SIZE ;i++) {
+			for(j=0;j < SIZE ;j++) {
 				grid[i][j] = i * SIZE + j +1;
 			}
 		}
-
-		System.out.println("Display Transpost Array : ");
-		for(int j=0;j<SIZE;j++) {
-			for(int i =0;i<SIZE;i++) {
-				System.out.printf("%2d",grid[i][j]);
-				System.out.printf("\t");
+		//Left
+		for(i =0 ; i< SIZE ;i++) {
+			for(j = 0;j<SIZE;j++){
+				System.out.print(grid[j][i]);
+				if(j!=SIZE-1) {
+					System.out.print("\t");
 				}
-				System.out.println();
-
-		}
-		System.out.println();
-		
-		System.out.println("Display Array Right : ");
-		int j = 0;
-		for(int i = 0; i < SIZE ;i++) {
-			if(i % 2 == 0) {
-				for(j = 0; j < SIZE ; j ++) {
-					System.out.printf("%2d",grid[i][j]);
-					System.out.printf("\t");
-
-				}
-				System.out.println();
 			}
-			if((i % 2) != 0) {
+			System.out.println();
+		}
+		
+		System.out.println("=========================================");
+		
+		for(i=0;i<SIZE;i++) {
+			if(i%2==0) {
+				for(j=0;j<SIZE;j++) {
+					System.out.print(grid[i][j]+" ");
+					if(j!=SIZE-1) {
+						System.out.print("\t");
+					}
+				}
+				System.out.println();
+			}else {
 				for(j=SIZE-1;j>=0;j--) {
-					System.out.printf("%2d",grid[i][j]);
-					System.out.printf("\t");
+					System.out.print(grid[i][j]+" ");
+					if(j!=0) {
+						System.out.print("\t");
+					}
 				}
 				System.out.println();
 			}
